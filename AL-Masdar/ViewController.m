3a31460a99c997cb3b41904eba7d2d7a7da91579
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-06-23 23:12:26 +0200

    1- Added the first country selecrion screen. 2- Added the selecting the user’s country as a first option.

--- AL-Masdar/ViewController.m
+++ AL-Masdar/ViewController.m
@@ -11,18 +11,49 @@
 #import "Reachability.h"
 #import "AFHTTPRequestOperationManager.h"
 #import <PQFCustomLoaders/PQFCustomLoaders.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+@interface ViewController ()<CLLocationManagerDelegate,UITableViewDelegate,UITableViewDataSource>
 @property (nonatomic, strong) PQFBouncingBalls *bouncingBalls;
 @end
 
 @implementation ViewController
 {
     NSMutableArray* dataSource;
+    CLLocationManager* locationManager;
+    CLLocation* currentLocation;
+    BOOL locationDone;
+    BOOL sourcesDone;
+    CLLocation* myLocation;
+    NSString* userCountry;
+    __weak IBOutlet UITableView *tableView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    dataSource = [[NSMutableArray alloc]init];
+    
+    [tableView setDelegate:self];
+    [tableView setNeedsDisplay];
+    
+    locationDone = NO;
+    sourcesDone = NO;
+    
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.distanceFilter = kCLDistanceFilterNone; //whenever we move
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if(IS_OS_8_OR_LATER) {
+        [locationManager requestWhenInUseAuthorization];
+    }
+    
+    [locationManager startUpdatingLocation];
+    
+    userCountry = @"";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,7 +69,7 @@
         [self performSegueWithIdentifier:@"NoConnectionSeg" sender:self];
     }else
     {
-        self.bouncingBalls = [PQFBouncingBalls createLoaderOnView:self.view];
+        self.bouncingBalls = [PQFBouncingBalls createLoaderOnView:tableView];
         self.bouncingBalls.jumpAmount = 50;
         self.bouncingBalls.zoomAmount = 20;
         self.bouncingBalls.separation = 20;
@@ -47,13 +78,49 @@
         
         AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
         manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-
+        
         
         [manager POST:@"http://moh2013.com/arabDevs/almasdar/getSources.php" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
             dataSource = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:nil];
-            [self.bouncingBalls removeLoader];
+            sourcesDone = YES;
+            if(locationDone && sourcesDone)
+            {
+                if(myLocation)
+                {
+                    ReverseGeocodeCountry *reverseGeocode = [[ReverseGeocodeCountry alloc] init];
+                    userCountry = [reverseGeocode getCountry:[myLocation coordinate].latitude :[myLocation coordinate].longitude];
+                    userCountry = [userCountry lowercaseString];
+                    BOOL found = NO;
+                    for(NSDictionary* dict in dataSource)
+                    {
+                        if([[dict objectForKey:@"countryEN"] isEqualToString:userCountry])
+                        {
+                            found = YES;
+                            userCountry = [dict objectForKey:@"countryAR"];
+                            break;
+                        }
+                    }
+                    if(!found)
+                    {
+                        userCountry = @"";
+                    }
+                    [self.bouncingBalls removeLoader];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [tableView reloadData];
+                        [tableView setNeedsDisplay];
+                    });
+                }
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [tableView reloadData];
+                [tableView setNeedsDisplay];
+            });
         }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-            [self.bouncingBalls removeLoader];
+            sourcesDone = YES;
+            if(locationDone && sourcesDone)
+            {
+                [self.bouncingBalls removeLoader];
+            }
             NSLog(@"Error: %@", error);
         }];
     }
@@ -66,5 +133,158 @@
     NetworkStatus networkStatus = [reachability currentReachabilityStatus];
     return networkStatus != NotReachable;
 }
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    if([userCountry isEqualToString:@""])
+    {
+        return 1;
+    }else
+    {
+        return 2;
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if([userCountry isEqualToString:@""])
+    {
+        return 2;
+    }else
+    {
+        if(section == 0)
+        {
+            return 1;
+        }else
+        {
+            return 2;
+        }
+    }
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableVieww cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString* cellID = @"sourcesCell";
+    UITableViewCell *cell = [tableVieww dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    
+    
+    if([userCountry isEqualToString:@""])
+    {
+        if(indexPath.row == 0)
+        {
+            [[cell textLabel]setText:@"المصادر العربية"];
+        }else
+        {
+            [[cell textLabel]setText:@"المصادر العالمية"];
+        }
+    }else
+    {
+        if(indexPath.section == 0 && indexPath.row == 0)
+        {
+            [[cell textLabel]setText:userCountry];
+        }
+        else if(indexPath.section == 1 && indexPath.row == 0)
+        {
+            [[cell textLabel]setText:@"المصادر العربية"];
+        }else if(indexPath.section == 1 && indexPath.row == 1)
+        {
+            [[cell textLabel]setText:@"المصادر العالمية"];
+        }
+    }
+    
+    
+    return cell;
+}
+
+-(NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if([userCountry isEqualToString:@""])
+    {
+        if(section == 0)
+        {
+            return @"المصادر المتاحة";
+        }
+    }else
+    {
+        if(section == 0)
+        {
+            return @"مصادر بالقرب منك";
+        }else
+        {
+            return @"المصادر الأخرى";
+        }
+    }
+    
+    return @"";
+}
+
+
+#pragma mark location delegate
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations
+{
+    NSLog(@"%@",[locations lastObject]);
+    locationDone = YES;
+    myLocation = [locations lastObject];
+    ReverseGeocodeCountry *reverseGeocode = [[ReverseGeocodeCountry alloc] init];
+    userCountry = [reverseGeocode getCountry:[[locations lastObject] coordinate].latitude :[[locations lastObject] coordinate].longitude];
+    userCountry = [userCountry lowercaseString];
+    BOOL found = NO;
+    if(locationDone && sourcesDone)
+    {
+        for(NSDictionary* dict in dataSource)
+        {
+            if([[dict objectForKey:@"countryEN"] isEqualToString:userCountry])
+            {
+                found = YES;
+                userCountry = [dict objectForKey:@"countryAR"];
+                break;
+            }
+        }
+        if(!found)
+        {
+            userCountry = @"";
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [tableView reloadData];
+            [tableView setNeedsDisplay];
+        });
+        [self.bouncingBalls removeLoader];
+    }
+    
+    [locationManager stopUpdatingLocation];
+}
+
+
+- (void)locationManager:(CLLocationManager*)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    switch (status) {
+        case kCLAuthorizationStatusNotDetermined: {
+            NSLog(@"User still thinking..");
+            locationDone = NO;
+        } break;
+        case kCLAuthorizationStatusDenied: {
+            locationDone = YES;
+            NSLog(@"User hates you");
+        } break;
+        case kCLAuthorizationStatusAuthorizedWhenInUse:{
+        } break;
+        default:
+            break;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+    locationDone = YES;
+}
+
 
 @end
