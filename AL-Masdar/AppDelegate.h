1ffb8a782a41deb4e6a9de500a32a3b836992154
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-06-23 19:44:10 +0200

    Initial Commit

---
+++ AL-Masdar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AL-Masdar
+//
+//  Created by Osama Rabie on 6/23/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AL-Masdar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AL-Masdar
+//
+//  Created by Osama Rabie on 6/23/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
